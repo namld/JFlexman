d6780d80d0e7893903eb401293caaa29a6daa797
Le Duy Nam <nam.leduy@gmail.com>
2014-11-12 22:15:13 +0700

    second commit

---
+++ JFlexman/JFlexman/Areas/JFlexman/Controllers/FormBuilderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JFlexman.Areas.JFlexman.Controllers
+{
+    public class FormBuilderController : Controller
+    {
+        //
+        // GET: /JFlexman/FormBuilder/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetSubLevels(string uniqueDataID, int? user)
+        {
+            List<object> subRows = new List<object>();
+            subRows.Add(new { p1 = 1, p2 = "p2s", p3 = new {aa = "toi ko can ai"}});
+            return Json(new { SubRows = subRows, bien2 = 999});
+        }
+
+    }
+}
---
+++ JFlexman/JFlexman/Areas/JFlexman/JFlexmanAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace JFlexman.Areas.JFlexman
+{
+    public class JFlexmanAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "JFlexman";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "JFlexman_default",
+                "JFlexman/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
